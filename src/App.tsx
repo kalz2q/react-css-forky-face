c6266dbe016a1a38fbcc020aa15ccde653526df4
kalz2q <katsu95i@gmail.com>
2022-09-19 02:28:11 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,24 @@ function App() {
     <div className="App">
       <div className="forky">
         <div className="face"></div>
+        <div className="face-mask1"></div>
+        <div className="fork1"></div>
+        <div className="fork2"></div>
+        <div className="fork3"></div>
+        <div className="fork4"></div>
+        <div className="eye1"><div className="pupil1"></div></div>
+        <div className="eye2"> <div className="pupil2"></div></div>
+        <div className="mouth1"></div>
+        <div className="mouth2"></div>
+        <div className="brow"></div>
+        <div className="brow-mask1"></div>
+        <div className="brow-mask2"></div>
+        <div className="mouth-mask1"></div>
+        <div className="mouth-mask2"></div>
+        <div className="handle"></div>
+        <div className="handle-mask1"></div>
+        <div className="handle-mask2"></div>
+
       </div>
     </div>
   );
